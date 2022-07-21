0a21a972d8dca8ed2e11f6cf901ff63099580d8c
Zenky <bzenky@gmail.com>
2022-07-20 21:15:38 -0300

    initial configs

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import { ThemeProvider } from 'styled-components'
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
+
 export function App() {
   return (
-    <h1>Znk</h1>
+    <ThemeProvider theme={defaultTheme}>
+      <h1>Znk</h1>
+      <GlobalStyle />
+    </ThemeProvider>
   )
 }
